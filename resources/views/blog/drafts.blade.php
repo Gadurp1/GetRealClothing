fc99a264c26bdc3186e2fb53a25f2aa5bec6c52e
Gadurp1 <anthonydsaviano@gmail.com>
2015-10-14 18:33:13 -0500

    Create drafts.blade.php

---
+++ resources/views/blog/drafts.blade.php
@@ -0,0 +1,81 @@
+@extends('welcome')
+@section('content')
+
+
+      
+        <!-- Latest blog posts start -->
+        <section id="news" class="module-small">
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12">
+
+                        <h2 class="module-title font-alt">Latest Posts</h2>
+                        <div class="module-subtitle font-serif">
+	Check Out What Get Real & Some of Our Homies Have Been Up to                        </div>
+
+                    </div>
+
+                </div><!-- .row -->
+
+                <div class="col-md-8">
+
+                    <!-- Post item start -->
+                    @foreach($blog->where('status','draft') as $item)
+                        <div class="mb-5">
+                                <a href="blog/{{$item->slug}}"><img class="" src="{{asset($item->image)}}" alt=""></a>
+                            <div class=" font-alt">
+                                <h2 class=""><a href="blog/{{$item->slug}}">{{$item->name}}</a></h2>
+                                <div class="post-meta">
+                                    By <a href="#">Mark Stone</a> |{{$item->created_at}} | 3 Comments
+                                </div>
+                            </div>
+                            <div class="post-entry">
+                            </div>
+                            <div class="post-more">
+                                <a href="blog/{{$item->slug}}" class="more-link">Read more</a>
+                            </div>
+
+                    	</div>
+                    	<br></br>
+                    	<!-- Post item end -->
+                    	@endforeach
+
+                    
+
+                </div><!-- .row -->
+
+<!-- Sidebar column start -->
+                    <div class="col-sm-4 col-md-3 col-md-offset-1 sidebar">
+
+                        <!-- Widget start -->
+                        <div class="widget">
+                            <form role="form">
+                                <div class="search-box">
+                                    <input type="text" class="form-control" placeholder="Search...">
+                                    <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- Widget end -->
+                    
+                        <!-- Widget start -->
+                        <div class="widget">
+                            <h5 class="widget-title font-alt">Check Use Out On Instagram</h5>
+
+						<!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script>
+						<iframe src="//instansive.com/widgets/3a9e0d6b9b6e5ff29ba9f020ea1b7018b957feb6.html" id="instansive_3a9e0d6b9b" name="instansive_3a9e0d6b9b"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+                        <!-- Widget start -->
+                        </div>
+                      
+
+          
+
+                    </div>
+                    <!-- Sidebar column end -->
+            </div>
+        </section>
+        <!-- Latest blog posts end -->
+    @stop
+      
